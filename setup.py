7f56f4bb61b35de4a9ee61b8c2ee0101e8d57e39
fivosts <fivos_ts@hotmail.com>
2020-02-18 17:17:05 +0000

    Config setup for web package

--- setup.py
+++ setup.py
@@ -1,1 +1,17 @@
+from setuptools import setup
 
+setup(name='web_crawler',
+      version='0.0.1',
+      description='Python utility to scrap data from websites',
+      url='http://github.com/fivosts/pinkySpeaker',
+      author='Foivos Tsimpourlas',
+      author_email='fivos_ts@hotmail.com',
+      license='MIT',
+      packages=['web'],
+      install_dir="~/.local/lib/python3.7/dist-packages/",
+      install_requires=[
+          'selenium',
+          'BeautifulSoup4',
+          'pandas'
+      ],
+      zip_safe=True)
