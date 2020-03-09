e381f8c7366e08d031b1000b0bcd3ae203457976
fivosts <fivos_ts@hotmail.com>
2020-03-09 19:26:37 +0000

    Start data loading method

--- app.py
+++ app.py
@@ -6,3 +6,14 @@ This is the entry point of the application.
 """
 from lib import speaker
 
+import argparse as arg
+
+def main():
+	p = arg.ArgumentParser(description = "Song generator machine learning models")
+	# p.add_argument()
+	args = p.parse_args()
+	return
+
+if __name__ == "__main__":
+	main()
+	exit(0)
--- lib/dataloader.py
+++ lib/dataloader.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python
+
+from eupy import mrcrawley as cr
+
+def gather_data():
+
+	# Make a map of pink floyd html and dataset folder
+	cr.crawlAZ("https://www.azlyrics.com/p/pinkfloyd.html", "./dataset/pink_floyd")
+	return
