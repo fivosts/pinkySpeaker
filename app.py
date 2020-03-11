c6eb9a8d8ca97dad883ac88111990e8f3969e8e7
fivosts <fivos_ts@hotmail.com>
2020-03-11 00:57:22 +0000

    Move functions to dataloader and comment

--- app.py
+++ app.py
@@ -5,51 +5,47 @@
 This is the entry point of the application.
 """
 from lib import dataloader as dl
+from lib import model as m
 
 import argparse as arg
 import logging
 import os
 
 """
-Checks the existence of requested dataset
+Core function.
 """
-def check_dataset(path):
-	for p in path:
-		basename = os.path.basename(p)
-		logging.info("Checking if {} dataset exists".format(basename))
-		if not nos.isdir(p):
-			logging.warning("{} dataset does not exist".format(basename))
-			logging.info("Extracting dataset for {}".format(basename))
-			dl.extract_artist(basename, p)
-		else:
-			logging.info("OK")
-	return
-
-"""
-Checks whether a default generative model exists
-"""
-def check_model(path):
-
-	return
-
 def main():
 	logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 	
 	p = arg.ArgumentParser(description = "Song generator machine learning models")
-	p.add_argument('-m', '--mode', default = "gen", choices = ["train", "gen"], required = False, help = "Choose between training a word model or generating songs")
-	p.add_argument('-t', '--train', default = [], required = False, help = "Train model on selected artists")
-	p.add_argument('-dp', '--datapath', default = "./dataset", required = False, help = "Path where datasets are stored")
-	p.add_argument('-mp', '--modelpath', default = "./model", required = False, help = "Path where models are stored")
+	p.add_argument('-m', '--mode', default = "gen", 
+					choices = ["train", "gen"], required = False, 
+					help = "Choose between training a word model or generating songs")
+	p.add_argument('-t', '--train', default = [], 
+					required = False, 
+					help = "Train model on selected artists")
+	p.add_argument('-dp', '--datapath', default = "./dataset", 
+					required = False, 
+					help = "Base path of datasets")
+	p.add_argument('-mp', '--modelpath', default = "./model", 
+					required = False, 
+					help = "Base path of models")
 
 	args = p.parse_args()
+
 	if args.mode == "train":
-		check_dataset([os.path.join(args.datapath, x) for x in args.train])
-		dl.
+		logging.info("Selected training of language model.")
+		artist_list = [os.path.join(args.datapath, x) for x in args.train]
+		dataset = fetch_data(artist_list)
+		model = m.simpleRNN(dataset)
 	else:
 		check_model(args.modelpath)
 
 	return
 
+"""
+Booting point of app
+"""
 if __name__ == "__main__":
 	main()
 	exit(0)
