716ccb5934afc3c2a2ef68155b778ff50a84a0bb
fivosts <fivos_ts@hotmail.com>
2020-03-12 18:39:28 +0000

    Extended functions, reordered dataset construction

--- lib/model.py
+++ lib/model.py
@@ -41,12 +41,6 @@ class simpleRNN:
         word_model = self._initWordModel(inp_sent)
         pretrained_weights = word_model.wv.vectors
 
-        title_set, lyric_set = self._constructTLSet(raw_data, max_title_length, all_titles_length)
-        self._data = { 'word_model'     : inp_sent,
-                       'title_model'    : title_set,
-                       'lyric_model'    : lyric_set 
-                     }
-
         title_model = self._initTitleModel(pretrained_weights)
         lyric_model = self._initLyricModel(pretrained_weights)
         self._model = { 'word_model'  : word_model,
@@ -54,6 +48,15 @@ class simpleRNN:
                         'lyric_model' : lyric_model 
                       }
         self._logger.info("SimpleRNN Compiled successfully")
+
+
+        title_set, lyric_set = self._constructTLSet(raw_data, max_title_length, all_titles_length)
+        self._data = { 'word_model'     : inp_sent,
+                       'title_model'    : title_set,
+                       'lyric_model'    : lyric_set 
+                     }
+
+                     
         return 
 
     def _initWordModel(self, inp_sentences):
@@ -111,8 +114,8 @@ class simpleRNN:
 
             curr_title_length = len(song['title'])
             all_titles_length += curr_title_length - 1
-            if curr_title_length > title_length:
-                title_length = curr_title_length
+            if curr_title_length > max_title_length:
+                max_title_length = curr_title_length
             
             for word in song['title']:
                 words.append(word)
@@ -122,6 +125,7 @@ class simpleRNN:
         return self._listToChunksList(words, sentence_size), max_title_length, all_titles_length
 
     def _constructTLSet(self, raw_data, max_title_length, all_titles_length):
+        self._logger.debug("pinkySpeaker.lib.model.simpleRNN._constructTLSet()")
 
         title_set = {'input': np.zeros([all_titles_length, max_title_length], dtype=np.int32), 
                 'output': np.zeros([all_titles_length], dtype=np.int32)}
@@ -133,38 +137,18 @@ class simpleRNN:
         lyric_expected_output = []
         
         for song in raw_data:
-            ## i will go from 0->15
-            ## i means: I will add this number of words. 1 word up to len()-1
-            for sindx in range(len(song['title']) - 1):
-                # print("i: {}".format(i))
-                ## k will go from 0->i+1
-                ## k means: if i am adding i words, start counting each one with k
-                for chindx in range(sindx + 1):
-                    # print("k: {}".format(k))
-                    # print(song['title'][k])
-                    # print(song['title'][k+1])
-                    # print(max_title_length - 1 + (k - i))
+            for curr_sent_size in range(len(song['title']) - 1):
+                for current_index in range(curr_sent_size + 1):
+                    title_set['input'][index][(max_title_length - 1) - curr_sent_size + current_index] = self.word2idx(song['title'][current_index])
+                    title_set['output'][index] = self.word2idx(song['title'][current_index + 1])
+                index += 1
 
-                    max_index = max_title_length - 1
-                    sentence_size = sindx
-                    current = chindx
-                    # print("Going to insert {} in position: {}".format(song['title'][k], max_index - sentence_size + current))
-
-                    title_set['input'][index][max_index - sentence_size + current] = word2idx(song['title'][chindx])
-                    title_set['output'][index] = word2idx(song['title'][chindx + 1])
-
-            flat_song = [song['title']] + song['lyrics']
-            flat_song = [" ".join(x) for x in flat_song]
-            flat_song = " ".join(flat_song).split()
+            # flat_song = [song['title']] + song['lyrics']
+            # flat_song = [" ".join(x) for x in [song['title']] + song['lyrics']]
+            flat_song = " ".join([" ".join(x) for x in [song['title']] + song['lyrics']]).split()
             for indx in range(len(flat_song) - 4):
-                lyric_input.append([word2idx(x) for x in flat_song[indx : indx + 4]])
-                expected_output.append(word2idx(flat_song[indx + 4]))
-
-        title_set = {'input': np.zeros([all_titles_length, max_title_length], dtype=np.int32), 
-                     'output': np.zeros([all_titles_length], dtype=np.int32)}
-
-        title_set['input'] = np.asarray(title_input, dtype = np.int32)
-        title_set['output'] = np.asarray(expected_title_output, dtype = np.int32)
+                lyric_input.append([self.word2idx(x) for x in flat_song[indx : indx + 4]])
+                lyric_expected_output.append(self.word2idx(flat_song[indx + 4]))
 
 
         lyric_set = {'input': np.zeros([len(lyric_input), 4], dtype=np.int32), 
@@ -176,6 +160,8 @@ class simpleRNN:
         return title_set, lyric_set
 
     def word2idx(self, word):
+        self._logger.debug("pinkySpeaker.lib.model.simpleRNN.word2idx()")
         return self._model['word_model'].wv.vocab[word].index
     def idx2word(self, idx):
+        self._logger.debug("pinkySpeaker.lib.model.simpleRNN.idx2word()")
         return self._model['word_model'].wv.index2word[idx]
