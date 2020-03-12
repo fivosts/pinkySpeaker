4f09659a2062fce6d23f3997eb4e6a1013c07390
fivosts <fivos_ts@hotmail.com>
2020-03-12 19:28:36 +0000

    Extended fit function

--- app.py
+++ app.py
@@ -58,6 +58,7 @@ def main():
 		artist_list = [os.path.join(args.datapath, x.lower()) for x in args.train]
 		dataset = dl.fetch_data(artist_list)
 		model = m.simpleRNN(data = dataset)
+		model.fit()
 	else:
 		#TODO
 		pass
