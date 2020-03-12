d92f0ca1c47e1c6670448eb0a43c1f24c41bfe83
fivosts <fivos_ts@hotmail.com>
2020-03-12 15:57:47 +0000

    Add functions for model initialization

--- lib/model.py
+++ lib/model.py
@@ -11,7 +11,29 @@ class simpleRNN:
 	def __init__(self, data = None):
 		self._logger = l.getLogger()
 		self._logger.debug("pinkySpeaker.lib.model.simpleRNN.__init__()")
-
+		self._initNNModel()
 		#struct_sentences is only used for the word model
 		# One function that will return title_set, lyric_set
 		return
+
+	def _initNNModel(self):
+		self._logger.debug("pinkySpeaker.lib.model.simpleRNN._initNNModel()")
+		self._logger.info("Initialize NN Model")
+		## Any new sub-model should be registered here
+		## The according function should be written
+		self._model = { 'word_model'  : self._setWordModel(),
+						'title_model' : self._setTitleModel(),
+						'lyric_model' : self._setLyricModel() 
+					  } 
+		return 
+
+	def _initWordModel(self):
+
+		return
+
+	def _initTitleModel(self):
+		return
+
+	def _initLyricModel(self):
+		return
+		
