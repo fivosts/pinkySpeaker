bea17bb1d0cec73e14876f6a366f99009c6133dc
fivosts <fivos_ts@hotmail.com>
2020-03-12 18:04:43 +0000

    Fix whitespace

--- lib/model.py
+++ lib/model.py
@@ -17,89 +17,89 @@ from keras.utils.data_utils import get_file
 
 class simpleRNN:
 
-	_logger = None
+    _logger = None
 
-	def __init__(self, data = None):
-		self._logger = l.getLogger()
-		self._logger.debug("pinkySpeaker.lib.model.simpleRNN.__init__()")
+    def __init__(self, data = None):
+        self._logger = l.getLogger()
+        self._logger.debug("pinkySpeaker.lib.model.simpleRNN.__init__()")
 
-		self._data = data
-		self._initNNModel()
-		#struct_sentences is only used for the word model
-		# One function that will return title_set, lyric_set
-		return
+        self._data = data
+        self._initNNModel()
+        #struct_sentences is only used for the word model
+        # One function that will return title_set, lyric_set
+        return
 
-	def _initNNModel(self):
-		self._logger.debug("pinkySpeaker.lib.model.simpleRNN._initNNModel()")
-		self._logger.info("Initialize NN Model")
-		## Any new sub-model should be registered here
-		## The according function should be written
-		word_model = self._initWordModel()
-		pretrained_weights = word_model.wv.vectors
+    def _initNNModel(self):
+        self._logger.debug("pinkySpeaker.lib.model.simpleRNN._initNNModel()")
+        self._logger.info("Initialize NN Model")
+        ## Any new sub-model should be registered here
+        ## The according function should be written
+        word_model = self._initWordModel()
+        pretrained_weights = word_model.wv.vectors
 
-		title_model = self._initTitleModel(pretrained_weights)
-		lyric_model = self._initLyricModel(pretrained_weights)
-		self._model = { 'word_model'  : word_model,
-						'title_model' : title_model,
-						'lyric_model' : lyric_model 
-					  }
-	    self._logger.info("SimpleRNN Compiled successfully")
-		return 
+        title_model = self._initTitleModel(pretrained_weights)
+        lyric_model = self._initLyricModel(pretrained_weights)
+        self._model = { 'word_model'  : word_model,
+                        'title_model' : title_model,
+                        'lyric_model' : lyric_model 
+                      }
+        self._logger.info("SimpleRNN Compiled successfully")
+        return 
 
-	def _initWordModel(self):
-		self._logger.debug("pinkySpeaker.lib.model.simpleRNN._initWordModel()")
-		inp_sent = self._constructSentences()
-		wm = gensim.models.Word2Vec(inp_sent, size = 300, min_count = 1, window = 4, iter = 200)
-		self._logger.info("Word2Vec word model initialized")
-		return wm
+    def _initWordModel(self):
+        self._logger.debug("pinkySpeaker.lib.model.simpleRNN._initWordModel()")
+        inp_sent = self._constructSentences()
+        wm = gensim.models.Word2Vec(inp_sent, size = 300, min_count = 1, window = 4, iter = 200)
+        self._logger.info("Word2Vec word model initialized")
+        return wm
 
-	def _initTitleModel(self, weights):
-		self._logger.debug("pinkySpeaker.lib.model.simpleRNN._initTitleModel()")
+    def _initTitleModel(self, weights):
+        self._logger.debug("pinkySpeaker.lib.model.simpleRNN._initTitleModel()")
 
-		vocab_size, embedding_size = weights.shape
-		tm = Sequential()
-		tm.add(Embedding(input_dim=vocab_size, output_dim=embedding_size, weights=[weights]))
-		tm.add(LSTM(units=2*embedding_size, return_sequences=True))
-		tm.add(LSTM(units=2*embedding_size))
-		tm.add(Dense(units=vocab_size))
-		tm.add(Activation('softmax'))
-		tm.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
+        vocab_size, embedding_size = weights.shape
+        tm = Sequential()
+        tm.add(Embedding(input_dim=vocab_size, output_dim=embedding_size, weights=[weights]))
+        tm.add(LSTM(units=2*embedding_size, return_sequences=True))
+        tm.add(LSTM(units=2*embedding_size))
+        tm.add(Dense(units=vocab_size))
+        tm.add(Activation('softmax'))
+        tm.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
 
-		self._logger.info("Title model initialized")
-		return tm
+        self._logger.info("Title model initialized")
+        return tm
 
-	def _initLyricModel(self, weights):
-		self._logger.debug("pinkySpeaker.lib.model.simpleRNN._initLyricModel()")
+    def _initLyricModel(self, weights):
+        self._logger.debug("pinkySpeaker.lib.model.simpleRNN._initLyricModel()")
 
-		vocab_size, embedding_size = weights.shape
-		tm = Sequential()
-		tm.add(Embedding(input_dim=vocab_size, output_dim=embedding_size, weights=[weights]))
-		tm.add(LSTM(units=2*embedding_size, return_sequences=True))
-		tm.add(LSTM(units=2*embedding_size))
-		tm.add(Dense(units=vocab_size))
-		tm.add(Activation('softmax'))
-		tm.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
+        vocab_size, embedding_size = weights.shape
+        tm = Sequential()
+        tm.add(Embedding(input_dim=vocab_size, output_dim=embedding_size, weights=[weights]))
+        tm.add(LSTM(units=2*embedding_size, return_sequences=True))
+        tm.add(LSTM(units=2*embedding_size))
+        tm.add(Dense(units=vocab_size))
+        tm.add(Activation('softmax'))
+        tm.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
 
-		self._logger.info("Lyric model initialized")
-		return tm
+        self._logger.info("Lyric model initialized")
+        return tm
 
-	def _listToChunksList(self, lst, n):
-		self._logger.debug("pinkySpeaker.lib.model.simpleRNN._listToChunksList()")
-		chunk_list = []
-		for i in range(0, len(lst), n):
-			chunk_list.append(lst[i: i + n])
-		return chunk_list
+    def _listToChunksList(self, lst, n):
+        self._logger.debug("pinkySpeaker.lib.model.simpleRNN._listToChunksList()")
+        chunk_list = []
+        for i in range(0, len(lst), n):
+            chunk_list.append(lst[i: i + n])
+        return chunk_list
 
-	def _constructSentences(self):
-		self._logger.debug("pinkySpeaker.lib.model.simpleRNN._constructSentences()")
-		self._logger.info("Sentence preprocessing for word model")
+    def _constructSentences(self):
+        self._logger.debug("pinkySpeaker.lib.model.simpleRNN._constructSentences()")
+        self._logger.info("Sentence preprocessing for word model")
 
-		sentence_size = 10
-		words = []
-		for song in self._data:
-			for word in song['title']:
-				words.append(word)
-			for sent in song['lyrics']:
-				for word in sent:
-					words.append(word)
-		return self._listToChunksList(words, sentence_size)
+        sentence_size = 10
+        words = []
+        for song in self._data:
+            for word in song['title']:
+                words.append(word)
+            for sent in song['lyrics']:
+                for word in sent:
+                    words.append(word)
+        return self._listToChunksList(words, sentence_size)
