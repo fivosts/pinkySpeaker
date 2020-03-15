1d063c1116ad09ca9f44d1d4cd5112f7262858ae
fivosts <fivos_ts@hotmail.com>
2020-03-15 02:32:50 +0000

    Synthetic working example

--- lib/test.py
+++ lib/test.py
@@ -1,28 +1,45 @@
 from numpy import array
+import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import TimeDistributed
+from keras.layers.embeddings import Embedding
 from keras.layers import LSTM
-from keras.layers import Embedding
+from keras.callbacks import LambdaCallback
 # prepare sequence
-length = 5
-seq = array([i/float(length) for i in range(length)])
-X = seq.reshape(1, length, 1)
-y = seq.reshape(1, length, 1)
+
+## i.e. samples
+num_songs = 126
+
+## i.e. num of timesteps 
+max_len_songs = 25
+
+## i.e. features
+vocab_size = 2917
+
+embedding_size = 300
+
+X = np.zeros((num_songs, max_len_songs))
+y = np.ones((num_songs, max_len_songs, vocab_size))
+
+# print(X1)
 # define LSTM configuration
-n_neurons = length
-n_batch = 1
-n_epoch = 10
+n_batch = 16
+n_epoch = 20
 # create LSTM
 model = Sequential()
-model.add(Embedding(input_dim=1, output_dim=10))
-model.add(LSTM(n_neurons, input_shape=(length, 10), return_sequences=True))
-model.add(TimeDistributed(Dense(1)))
+model.add(Embedding(input_dim = vocab_size, output_dim = embedding_size))
+model.add(LSTM(2*embedding_size, input_shape=(max_len_songs, embedding_size), return_sequences=True))
+model.add(LSTM(2*embedding_size, input_shape=(max_len_songs, 600), return_sequences=True))
+model.add(TimeDistributed(Dense(vocab_size)))
 model.compile(loss='mean_squared_error', optimizer='adam')
 print(model.summary())
 # train LSTM
-model.fit(X, y, epochs=n_epoch, batch_size=n_batch, verbose=2)
+model.fit(X, y, epochs=n_epoch, batch_size=n_batch)
 # evaluate
 result = model.predict(X, batch_size=n_batch, verbose=0)
 for value in result[0,:,0]:
 	print('%.1f' % value)
+
+
+
