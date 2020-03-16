f65fc419e6710cd1e62e4b38089e3cf1e746b3fb
fivosts <fivos_ts@hotmail.com>
2020-03-16 13:35:19 +0000

    Add data sampling

--- lib/dataloader.py
+++ lib/dataloader.py
@@ -4,23 +4,30 @@ import sys, os, re
 from os import path as pt
 sys.path.append(pt.dirname("/home/fivosts/PhD/Code/eupy/eupy"))
 from eupy.mrcrawley import spider as cr
-
 from eupy.native import logger as l
+from eupy.native import plotter as plt
+
+"""
+Plot length of samples as bars.
+Useful to determine the fixed size of sequences that will be fed to LSTM.
+"""
+def plotSamples(data):
+	return
 
 """
 Send crawl request to gather artist data.
 Returns specific artist dataset.
 """
-def fetch_artist(artist):
-	l.getLogger().debug("pinkySpeaker.lib.dataloader.fetch_artist()")
+def fetchArtist(artist):
+	l.getLogger().debug("pinkySpeaker.lib.dataloader.fetchArtist()")
 	data = cr.crawl(artist)
 	return data
 
 """
 Reconstruct input sentence and return it.
 """
-def pruned_sentence(sentence):
-	l.getLogger().debug("pinkySpeaker.lib.dataloader.pruned_sentence()")
+def prunedSentences(sentence):
+	l.getLogger().debug("pinkySpeaker.lib.dataloader.prunedSentences()")
 	return re.sub(".*?\[(.*?)\]", "", sentence)\
 			.lower()\
 			.replace("i'm", "i am").replace("it's", "it is")\
@@ -37,14 +44,14 @@ def pruned_sentence(sentence):
 """
 Reads a single song file and returns a dictionary with artist, title, lyric section.
 """
-def read_file(song_path):
-	l.getLogger().debug("pinkySpeaker.lib.dataloader.read_file()")
+def readFile(song_path):
+	l.getLogger().debug("pinkySpeaker.lib.dataloader.readFile()")
 	
 	with open(song_path, 'r') as f:
 		song = []
 		for line in f:
 			if line != "\n":
-				sentence = pruned_sentence(line)
+				sentence = prunedSentences(line)
 				if sentence:
 					song.append(sentence)
 		# Add endfile token to the end
@@ -57,20 +64,20 @@ def read_file(song_path):
 """
 Iterates over all song files of a specific artist.
 """
-def read_dataset(artist_path):
-	l.getLogger().debug("pinkySpeaker.lib.dataloader.read_dataset()")
+def readDataset(artist_path):
+	l.getLogger().debug("pinkySpeaker.lib.dataloader.readDataset()")
 
 	dataset = []
 	for file in os.listdir(artist_path):
 		file_path = pt.join(artist_path, file)
-		dataset.append(read_file(file_path))
+		dataset.append(readFile(file_path))
 	return dataset
 
 """
 Boolean check whether requested artist's dataset exists.
 """
-def dataset_exists(path, basename):
-	l.getLogger().debug("pinkySpeaker.lib.dataloader.dataset_exists()")
+def datasetExists(path, basename):
+	l.getLogger().debug("pinkySpeaker.lib.dataloader.datasetExists()")
 
 	l.getLogger().info("Check if {} dataset exists.".format(basename))
 	if not pt.isdir(path):
@@ -96,18 +103,18 @@ def writeToFiles(data, artist_path):
 Boot function.
 Gets a requested artist and returns its data.
 """
-def fetch_data(artist_path_list):
+def fetchData(artist_path_list):
 	l.getLogger().debug("pinkySpeaker.lib.dataloader.fetch_data()")
 
 	l.getLogger().info("Fetch data of artist list.")
 	for artist_path in artist_path_list:
 		l.getLogger().info(artist_path)
 		basename = pt.basename(artist_path)
-		if not dataset_exists(artist_path, basename):
+		if not datasetExists(artist_path, basename):
 			l.getLogger().info("Extract dataset for {}.".format(basename))		
-			data = fetch_artist(basename)
+			data = fetchArtist(basename)
 			writeToFiles(data)
 		else:
 			l.getLogger().info("OK")
-			data = read_dataset(artist_path)
+			data = readDataset(artist_path)
 	return data
