86f9eae14321526dc42bc77a0e8431682cc9959b
fivosts <fivos_ts@hotmail.com>
2020-03-18 23:55:46 +0000

    Finally full song as a sample is working

--- lib/model.py
+++ lib/model.py
@@ -16,6 +16,8 @@ from keras.layers import Dense, Activation, TimeDistributed, Flatten
 from keras.models import Sequential, load_model
 from keras.utils.data_utils import get_file
 
+from keras.utils import to_categorical
+
 class simpleRNN:
 
     _logger = None
@@ -39,8 +41,8 @@ class simpleRNN:
     def _initArchitecture(self, raw_data):
         self._logger.debug("pinkySpeaker.lib.model.simpleRNN._initArchitecture()")
 
-        max_title_length, all_titles_length, inp_sentences = self._initNNModel(raw_data)
-        self._initDataset(raw_data, max_title_length, all_titles_length, inp_sentences)
+        vocab_size, max_title_length, all_titles_length, inp_sentences = self._initNNModel(raw_data)
+        self._initDataset(raw_data, vocab_size, max_title_length, all_titles_length, inp_sentences)
 
         return
 
@@ -51,7 +53,7 @@ class simpleRNN:
         inp_sent, max_title_length, all_titles_length = self._constructSentences(raw_data)
         word_model = self._initWordModel(inp_sent)
         pretrained_weights = word_model.wv.vectors
- 
+        vocab_size, _ = pretrained_weights.shape
         ## Any new sub-model should be registered here
         ## The according function should be written to initialize it
         self._model = { 'word_model'  : word_model,
@@ -60,12 +62,12 @@ class simpleRNN:
                       }
         self._logger.info("SimpleRNN Compiled successfully")
 
-        return max_title_length, all_titles_length, inp_sent
+        return vocab_size, max_title_length, all_titles_length, inp_sent
 
-    def _initDataset(self, raw_data, mx_t_l, all_t_l, inp_sent):
+    def _initDataset(self, raw_data, vocab_size, mx_t_l, all_t_l, inp_sent):
         self._logger.debug("pinkySpeaker.lib.model.simpleRNN._initDataset()")
 
-        title_set, lyric_set = self._constructTLSet(raw_data, mx_t_l, all_t_l)
+        title_set, lyric_set = self._constructTLSet(raw_data, vocab_size, mx_t_l, all_t_l)
 
         if len(title_set['input']) != len(title_set['output']):
             raise ValueError("Wrong title set dimensions!")
@@ -99,6 +101,7 @@ class simpleRNN:
         tm.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
 
         self._logger.info("Title model initialized")
+        self._logger.info(tm.summary())
         return tm
 
     def _initLyricModel(self, weights):
@@ -106,19 +109,19 @@ class simpleRNN:
 
         vocab_size, embedding_size = weights.shape
 
-        tm = Sequential()
-        tm.add(Embedding(input_dim=vocab_size, output_dim=embedding_size, trainable = False, weights=[weights]))
-        tm.add(LSTM(units=2*embedding_size, input_shape = (self._lyric_sequence_length, embedding_size), return_sequences=True))
-        tm.add(LSTM(units=2*embedding_size, input_shape = (self._lyric_sequence_length, 2*embedding_size), return_sequences = True))
+        lm = Sequential()
+        lm.add(Embedding(input_dim=vocab_size, output_dim=embedding_size, trainable = False, weights=[weights]))
+        lm.add(LSTM(units=2*embedding_size, input_shape = (None, embedding_size), return_sequences=True))
+        lm.add(LSTM(units=2*embedding_size, input_shape = (None, 2*embedding_size), return_sequences = True))
         # tm.add(Flatten())
-        tm.add(TimeDistributed(Dense(units=vocab_size, activation = 'softmax')))
+        lm.add(TimeDistributed(Dense(units=vocab_size, activation = 'softmax')))
         # tm.add(Flatten())
         # tm.add(Dense(units = vocab_size, activation = 'softmax'))
         # tm.add(Activation('softmax'))
-        tm.compile(optimizer='adam', loss='mean_squared_error')
-        print(tm.summary())
+        lm.compile(optimizer='adam', loss='categorical_crossentropy')
         self._logger.info("Lyric model initialized")
-        return tm
+        self._logger.info(lm.summary())
+        return lm
 
     def _listToChunksList(self, lst, n):
         self._logger.debug("pinkySpeaker.lib.model.simpleRNN._listToChunksList()")
@@ -149,17 +152,17 @@ class simpleRNN:
                     words.append(word)
         return self._listToChunksList(words, sentence_size), max_title_length, all_titles_length
 
-    def _constructTLSet(self, raw_data, max_title_length, all_titles_length):
+    def _constructTLSet(self, raw_data, vocab_size, max_title_length, all_titles_length):
         self._logger.debug("pinkySpeaker.lib.model.simpleRNN._constructTLSet()")
 
         title_set = {'input': np.zeros([all_titles_length, max_title_length], dtype=np.int32), 
                      'output': np.zeros([all_titles_length], dtype=np.int32)}
 
         index = 0
-        lyric_inputs = []
-        lyric_expected_outputs = []
+        lyric_inputs = np.zeros([len(raw_data), self._lyric_sequence_length])
+        lyric_expected_outputs = np.zeros([len(raw_data), self._lyric_sequence_length, vocab_size])
         
-        for song in raw_data:
+        for idx, song in enumerate(raw_data):
             for curr_sent_size in range(len(song['title']) - 1):
                 for current_index in range(curr_sent_size + 1):
                     title_set['input'][index][(max_title_length - 1) - curr_sent_size + current_index] = self.word2idx(song['title'][current_index])
@@ -171,31 +174,84 @@ class simpleRNN:
 
             l_in, l_out = self._splitSongtoSentence(" ".join([" ".join(x) for x in ([song['title']] + song['lyrics'])]).split())
 
-            lyric_inputs += l_in
-            lyric_expected_outputs += l_out
+            # For loop is ok
+            for inp, out in zip(l_in, l_out):
+                # print(np.asarray([self.word2idx(x) for x in inp]))
+                # print(np.asarray([self.word2idx(x) for x in out]))
 
-            # flat_song = " ".join([" ".join(x) for x in song['lyrics']]).split()
+                ## Lyric inputs are ok
+                lyric_inputs[idx] = np.asarray([self.word2idx(x) for x in inp])
+                lyric_expected_outputs[idx] = self._softmax(np.asarray([self.idx2onehot(self.word2idx(x), vocab_size) for x in out]))
+
+
+                # print(self._softmax(np.asarray([self.idx2onehot(self.word2idx(x), vocab_size) for x in out])))
+                # print(self._softmax(np.asarray([self.idx2onehot(0, vocab_size) for x in out])))
+                # lyric_inputs += np.asarray([self.word2idx(x) for x in inp])
+                # lyric_expected_outputs += np.asarray([self.word2idx(x) for x in out])
+
+            # lyric_inputs.append(np.asarray([self.word2idx(x) for x in l_in]))
+            # lyric_expected_outputs.append(np.asarray([self.word2idx(x) for x in l_out]))
+
+            # flat_song = " ".join([" ".join([self.word2idx(token) for token in line]) for line in song['lyrics']]).split()
+            # ind_song = " ".join([ " ".join([self.word2idx(token) for token in line]) for line in song['lyrics']]).split()
+            # ind_song = [self.word2idx(tok) for line in [song['title']] + song['lyrics'] for tok in line]
+
+            # lyric_inputs.append(np.asarray(ind_song[:-1]))
+            # lyric_expected_outputs.append( np.asarray([ self.idx2onehot(x, 2917) for x in ind_song[1:] ]))
+
+
             # for indx in range(len(flat_song) - 4):
             #     lyric_inputs.append([self.word2idx(x) for x in flat_song[indx : indx + 4]])
             #     lyric_expected_outputs.append(self.word2idx(flat_song[indx + 4]))
 
+        # print(" ".join([self.idx2word(int(x)) for x in lyric_inputs[0]]))
+        # print(" ".join([self.idx2word(np.argmax(x)) for x in lyric_expected_outputs[0]]))
+        # exit(1)
 
-        lyric_set = {'input': np.zeros([len(lyric_inputs), 4], dtype=np.int32), 
-                     'output': np.zeros([len(lyric_inputs)], dtype=np.int32)}
 
-        lyric_set['input'] = np.asarray(lyric_inputs, dtype = np.int32)
-        lyric_set['output'] = np.asarray(lyric_expected_outputs, dtype = np.int32)
+        print(lyric_inputs.shape)
+        print(lyric_expected_outputs.shape)
+
+        lyric_set = {'input': lyric_inputs, 
+                     'output': lyric_expected_outputs}
+
+
+        # print(" ".join([self.idx2word(int(x)) for x in lyric_set['input'][10]]))
+        # print(" ".join([self.idx2word(np.argmax(x)) for x in lyric_set['output'][10]]))
+        # exit(1)
+
+        # lyric_set['input'] = np.asarray(lyric_inputs, dtype = np.int32)
+        # lyric_set['output'] = np.asarray(lyric_expected_outputs, dtype = np.int32)
 
         return title_set, lyric_set
+
+    def _softmax(self, inp_tensor):
+        m = np.max(inp_tensor)
+        e = np.exp(inp_tensor - m)
+        return e / np.sum(e)
 
     def _splitSongtoSentence(self, song_list):
         self._logger.debug("pinkySpeaker.lib.model.simpleRNN._splitSongtoSentence()")
         
-        print(song_list)
-        # TODO
-        exit(1)
+        l_in = [song_list[x : min(len(song_list), x + self._lyric_sequence_length)] for x in range(0, len(song_list), self._lyric_sequence_length)]
+        l_out = [song_list[x + 1 : min(len(song_list), x + 1 + self._lyric_sequence_length)] for x in range(0, len(song_list), self._lyric_sequence_length)]
+        l_in[-1] += ['endfile'] * (self._lyric_sequence_length - len(l_in[-1]))
+        l_out[-1] += ['endfile'] * (self._lyric_sequence_length - len(l_out[-1]))
 
+        # Works fine!
         return l_in, l_out
+
+    def _dtLyricGenerator(self):
+
+        while True:
+            sequence_length = np.random.randint(10, 100)
+            x_train = np.random.random((1000, sequence_length, 5))
+            # y_train will depend on past 5 timesteps of x
+            y_train = x_train[:, :, 0]
+            for i in range(1, 5):
+                y_train[:, i:] += x_train[:, :-i, i]
+            y_train = to_categorical(y_train > 2.5)
+            yield x_train, y_train
 
     def word2idx(self, word):
         self._logger.debug("pinkySpeaker.lib.model.simpleRNN.word2idx()")
@@ -203,32 +259,61 @@ class simpleRNN:
     def idx2word(self, idx):
         self._logger.debug("pinkySpeaker.lib.model.simpleRNN.idx2word()")
         return self._model['word_model'].wv.index2word[idx]
+    def idx2onehot(self, idx, size):
+        self._logger.debug("pinkySpeaker.lib.model.simpleRNN.idx2onehot()")
+        ret = np.zeros(size)
+        ret[idx] = 1000
+        return ret
 
     def fit(self, save_model = None):
         self._logger.debug("pinkySpeaker.lib.model.simpleRNN.fit()")
 
-        title_hist = self._model['title_model'].fit(self._dataset['title_model']['input'], 
-                                                    self._dataset['title_model']['output'],
-                                                    batch_size = 128,
-                                                    epochs = 2,
-                                                    callbacks = [LambdaCallback(on_epoch_end=self._title_per_epoch)] )
+        # title_hist = self._model['title_model'].fit(self._dataset['title_model']['input'], 
+        #                                             self._dataset['title_model']['output'],
+        #                                             batch_size = 128,
+        #                                             epochs = 2,
+        #                                             callbacks = [LambdaCallback(on_epoch_end=self._title_per_epoch)] )
 
-        print(self._dataset['lyric_model']['input'])
-        print(self._dataset['lyric_model']['input'].shape)
+        # lyric_hist = self._model['lyric_model'].fit(test_in,
+        #                                             test_out,
+        #                                             batch_size = 4,
+        #                                             epochs = 2,
+        #                                             callbacks = [LambdaCallback(on_epoch_end=self._lyrics_per_epoch)] )
 
-        # synthetic_in, synthetic_out = self._generateSynthetic()
 
-        test_in = np.zeros((126, 55))
-        test_out = np.zeros((126, 55, 2917))
+        # print("START")
+        # test_indx = 80
+        # print(self._dataset['lyric_model']['input'][test_indx].shape)
+        # print(self._dataset['lyric_model']['output'][test_indx].shape)
+        # # print(self._dataset['lyric_model']['input'][test_indx])
+        # print(" ".join([self.idx2word(int(x)) for x in self._dataset['lyric_model']['input'][test_indx]]))
+        # # print(self._dataset['lyric_model']['output'][test_indx])
+        # temp_list = []
+        # for token_vec in self._dataset['lyric_model']['output'][test_indx]:
+        #     print(token_vec.shape)
+        #     temp_list.append(self.idx2word(self._sample(token_vec)))
+        # print(" ".join(temp_list))
 
-        print(test_in.shape)
-        print(test_out.shape)
+        # print(" ".join([self.idx2word(int(x)) for x in self._dataset['lyric_model']['input'][80]]))
+        # print(" ".join([self.idx2word(np.argmax(x)) for x in self._dataset['lyric_model']['output'][80]]))
 
-        lyric_hist = self._model['lyric_model'].fit(test_in, 
-                                                    test_out,
-                                                    batch_size = 16,
-                                                    epochs = 2,
+        # for id in range(126):
+        #     print("YAY")
+        #     x = [self.idx2word(int(x)) for x in self._dataset['lyric_model']['input'][id]]
+        #     y = [self.idx2word(np.argmax(x)) for x in self._dataset['lyric_model']['output'][id]]
+        #     for i in range(319):
+        #         assert(x[i + 1] == y[i])
+
+        # exit(1)
+
+
+
+        lyric_hist = self._model['lyric_model'].fit(self._dataset['lyric_model']['input'],
+                                                    self._dataset['lyric_model']['output'],
+                                                    batch_size = 4,
+                                                    epochs = 30,
                                                     callbacks = [LambdaCallback(on_epoch_end=self._lyrics_per_epoch)] )
+       
         if save_model:
             save_model = pt.join(save_model, "simpleRNN")
             makedirs(save_model, exist_ok = True)
@@ -244,7 +329,7 @@ class simpleRNN:
         synthetic_in, synthetic_out = [], []
         songs = []
 
-        print(len(self._raw_data))
+        # print(len(self._raw_data))
         for song in self._raw_data:
             songindx = [self.word2idx(x) for x in song['title']]
             for line in song['lyrics']:
@@ -296,31 +381,48 @@ class simpleRNN:
             print('%s... -> %s' % (text, sample))
         return
 
-    def _generate_next(self, text, model, title, num_generated=140):
+    def _generate_next(self, text, model, title, num_generated=320):
         self._logger.debug("pinkySpeaker.lib.model.simpleRNN._generate_next()")
 
         word_idxs = [self.word2idx(word) for word in text.lower().split()]
         init_endline_bias = model.layers[-2].weights[1][self.word2idx("endline")]
         init_endfile_bias = model.layers[-2].weights[1][self.word2idx("endfile")]
+        # print(text)
         for i in range(num_generated):
             prediction = model.predict(x=np.array(word_idxs))
+            # print(prediction.shape)
+            # print(prediction[-1][0].shape)
+            max_cl = 0
+            max_indx = 0
+            for ind, item in enumerate(prediction[-1][0]):
+                if item > max_cl:
+                    max_cl = item
+                    max_indx = ind
 
-            idx = self._sample(prediction[-1], temperature=0.7)
+
+            # print(self.idx2word(max_indx))
+            # print(max_indx)
+            idx = self._sample(prediction[-1][0], temperature=0.7)
+            if idx == 0:
+                print("WARNING!!!\n\n\n\n\n\n\n\n\n\n")
+            if self.idx2word(idx) == "endfile":
+                print("EDNFILLLELELELELLE \n\n\n\n\n\n\n")
             word_idxs.append(idx)
             if (title == True and (self.idx2word(idx) == "endline" or self.idx2word(idx) == "endfile")) or (title == False and self.idx2word(idx) == "endfile"):
                 break
             else:
-                if self.idx2word(idx) == "endline":
-                    K.set_value(model.layers[-2].weights[1][self.word2idx("endline")], init_endline_bias)
+            	pass
+                # if self.idx2word(idx) == "endline":
+                #     K.set_value(model.layers[-2].weights[1][self.word2idx("endline")], init_endline_bias)
 
-                if title == True:
-                    b = 2
-                else:
-                    b = 0.2
-                K.set_value(model.layers[-2].weights[1][self.word2idx("endline")], model.layers[-2].weights[1][self.word2idx("endline")] + b*abs(model.layers[-2].weights[1][self.word2idx("endline")]))
-                K.set_value(model.layers[-2].weights[1][self.word2idx("endfile")], model.layers[-2].weights[1][self.word2idx("endfile")] + 0.4*abs(model.layers[-2].weights[1][self.word2idx("endfile")]))
-        K.set_value(model.layers[-2].weights[1][self.word2idx("endline")], init_endline_bias)
-        K.set_value(model.layers[-2].weights[1][self.word2idx("endfile")], init_endfile_bias)
+                # if title == True:
+                #     b = 2
+                # else:
+                #     b = 0.2
+        #         K.set_value(model.layers[-2].weights[1][self.word2idx("endline")], model.layers[-2].weights[1][self.word2idx("endline")] + b*abs(model.layers[-2].weights[1][self.word2idx("endline")]))
+        #         K.set_value(model.layers[-2].weights[1][self.word2idx("endfile")], model.layers[-2].weights[1][self.word2idx("endfile")] + 0.4*abs(model.layers[-2].weights[1][self.word2idx("endfile")]))
+        # K.set_value(model.layers[-2].weights[1][self.word2idx("endline")], init_endline_bias)
+        # K.set_value(model.layers[-2].weights[1][self.word2idx("endfile")], init_endfile_bias)
 
         return ' '.join(self.idx2word(idx) for idx in word_idxs)
 
