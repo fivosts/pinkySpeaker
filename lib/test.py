1006eb22bbf942450070e8c198f44da50cf340d9
fivosts <fivos_ts@hotmail.com>
2020-03-27 00:57:45 +0000

    Add transformer little test

--- lib/test.py
+++ lib/test.py
@@ -1,30 +1,74 @@
-from keras.models import Sequential
-from keras.layers import LSTM, Dense, TimeDistributed
-from keras.utils import to_categorical
 import numpy as np
+from keras_transformer import get_model
+from keras_transformer import decode
 
-model = Sequential()
+# Build a small toy token dictionary
+tokens = 'all work and no play makes jack a dull boy'.split(' ')
+token_dict = {
+    '<PAD>': 0,
+    '<START>': 1,
+    '<END>': 2,
+}
+for token in tokens:
+    if token not in token_dict:
+        token_dict[token] = len(token_dict)
 
-model.add(LSTM(32, return_sequences=True, input_shape=(None, 5)))
-model.add(LSTM(8, return_sequences=True))
-model.add(TimeDistributed(Dense(2, activation='sigmoid')))
+# Generate toy data
+encoder_inputs_no_padding = []
+encoder_inputs, decoder_inputs, decoder_outputs = [], [], []
+for i in range(1, len(tokens) - 1):
+    encode_tokens, decode_tokens = tokens[:i], tokens[i:]
+    encode_tokens = ['<START>'] + encode_tokens + ['<END>'] + ['<PAD>'] * (len(tokens) - len(encode_tokens))
+    output_tokens = decode_tokens + ['<END>', '<PAD>'] + ['<PAD>'] * (len(tokens) - len(decode_tokens))
+    decode_tokens = ['<START>'] + decode_tokens + ['<END>'] + ['<PAD>'] * (len(tokens) - len(decode_tokens))
+    encode_tokens = list(map(lambda x: token_dict[x], encode_tokens))
+    decode_tokens = list(map(lambda x: token_dict[x], decode_tokens))
+    output_tokens = list(map(lambda x: [token_dict[x]], output_tokens))
+    encoder_inputs_no_padding.append(encode_tokens[:i + 2])
+    encoder_inputs.append(encode_tokens)
+    decoder_inputs.append(decode_tokens)
+    decoder_outputs.append(output_tokens)
 
-print(model.summary(90))
+# Build the model
+model = get_model(
+    token_num=len(token_dict),
+    embed_dim=30,
+    encoder_num=3,
+    decoder_num=2,
+    head_num=3,
+    hidden_dim=120,
+    attention_activation='relu',
+    feed_forward_activation='relu',
+    dropout_rate=0.05,
+    embed_weights=np.random.random((13, 30)),
+)
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+)
+model.summary()
 
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam')
+# Train the model
+model.fit(
+    x=[np.asarray(encoder_inputs * 1000), np.asarray(decoder_inputs * 1000)],
+    y=np.asarray(decoder_outputs * 1000),
+    epochs=3,
+)
 
-def train_generator():
-    while True:
-        sequence_length = np.random.randint(10, 100)
-        x_train = np.random.random((1000, sequence_length, 5))
-        # y_train will depend on past 5 timesteps of x
-        y_train = x_train[:, :, 0]
-        for i in range(1, 5):
-            y_train[:, i:] += x_train[:, :-i, i]
-        y_train = to_categorical(y_train > 2.5)
-        print(x_train.shape)
-        print(y_train.shape)
-        yield x_train, y_train
 
-model.fit_generator(train_generator(), steps_per_epoch=30, epochs=10, verbose=1)
+print(encoder_inputs_no_padding)
+
+decoded = decode(
+    model,
+    encoder_inputs_no_padding,
+    start_token=token_dict['<START>'],
+    end_token=token_dict['<END>'],
+    pad_token=token_dict['<PAD>'],
+    max_len=100,
+)
+print(decoded)
+token_dict_rev = {v: k for k, v in token_dict.items()}
+for i in range(len(decoded)):
+    print(' '.join(map(lambda x: token_dict_rev[x], encoder_inputs_no_padding[i])))
+    print(' '.join(map(lambda x: token_dict_rev[x], decoded[i])))
+
