9ae5388d9c27222ce00b5cf3366dfe2dda8bb6a1
fivosts <fivos_ts@hotmail.com>
2020-03-27 01:19:46 +0000

    Added a larger test set

--- lib/test.py
+++ lib/test.py
@@ -3,7 +3,65 @@ from keras_transformer import get_model
 from keras_transformer import decode
 
 # Build a small toy token dictionary
-tokens = 'all work and no play makes jack a dull boy'.split(' ')
+tokens = 'The dominant sequence transduction models are based on complex recurrent or convolutional\
+            neural networks in an encoder-decoder configuration. The best performing models also connect\
+            the encoder and decoder through an attention mechanism. We propose a new simple network architecture,\
+            the Transformer, based solely on attention mechanisms, dispensing with recurrence and convolutions entirely.\
+            Experiments on two machine translation tasks show these models to be superior in quality while being more \
+            parallelizable and requiring significantly less time to train. Our model achieves 28.4 BLEU on the WMT \
+            2014 English-to-German translation task, improving over the existing best results, including ensembles by over 2 BLEU. \
+            On the WMT 2014 English-to-French translation task, our model establishes a new single-model state-of-the-art BLEU \
+            score of 41.8 after training for 3.5 days on eight GPUs, a small fraction of the training costs of the best models \
+            from the literature. We show that the Transformer generalizes well to other tasks by applying it successfully to English \
+            constituency parsing both with large and limited training data. '.replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .replace("  ", " ")\
+                                                                             .split(' ')
+
+
+
+
+
 token_dict = {
     '<PAD>': 0,
     '<START>': 1,
@@ -32,15 +90,15 @@ for i in range(1, len(tokens) - 1):
 # Build the model
 model = get_model(
     token_num=len(token_dict),
-    embed_dim=30,
-    encoder_num=3,
-    decoder_num=2,
-    head_num=3,
-    hidden_dim=120,
+    embed_dim=300,
+    encoder_num=5,
+    decoder_num=4,
+    head_num=6,
+    hidden_dim=256,
     attention_activation='relu',
     feed_forward_activation='relu',
     dropout_rate=0.05,
-    embed_weights=np.random.random((13, 30)),
+    embed_weights=np.random.random((122, 300)),
 )
 model.compile(
     optimizer='adam',
@@ -50,9 +108,9 @@ model.summary()
 
 # Train the model
 model.fit(
-    x=[np.asarray(encoder_inputs * 1000), np.asarray(decoder_inputs * 1000)],
-    y=np.asarray(decoder_outputs * 1000),
-    epochs=3,
+    x=[np.asarray(encoder_inputs * 10), np.asarray(decoder_inputs * 10)],
+    y=np.asarray(decoder_outputs * 10),
+    epochs=20,
 )
 
 
@@ -64,11 +122,13 @@ decoded = decode(
     start_token=token_dict['<START>'],
     end_token=token_dict['<END>'],
     pad_token=token_dict['<PAD>'],
-    max_len=100,
+    max_len=200,
 )
 print(decoded)
 token_dict_rev = {v: k for k, v in token_dict.items()}
 for i in range(len(decoded)):
+    print("SEQ START")
     print(' '.join(map(lambda x: token_dict_rev[x], encoder_inputs_no_padding[i])))
     print(' '.join(map(lambda x: token_dict_rev[x], decoded[i])))
+    print("SEQ END")
 
